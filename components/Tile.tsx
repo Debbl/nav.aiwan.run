0faf5b08c2f9e3356904cea405a661261363d9b0
Debbl <me@aiwan.run>
2023-03-24 15:21:11 +0800

    feat: add logo and title

--- components/Tile.tsx
+++ components/Tile.tsx
@@ -10,7 +10,7 @@ interface IProps {
 function Tile(props: IProps) {
   const { tile } = props;
   return (
-    <div className="inline-block w-80 rounded-xl border-2 p-2 hover:scale-105">
+    <div className="inline-block w-80 rounded-xl border-2 p-2">
       <a title={tile.name} href={tile.link} className="flex items-center gap-3">
         <div>
           <Image
