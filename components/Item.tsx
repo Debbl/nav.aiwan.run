f755a69077c8b44efb9b293234b8fbe768d129e3
Debbl <me@aiwan.run>
2023-03-25 15:49:58 +0800

    chore: css

--- components/Item.tsx
+++ components/Item.tsx
@@ -15,8 +15,8 @@ interface IProps {
 function Item(props: IProps) {
   const { item } = props;
   return (
-    <div className="mt-6 grid px-6 md:px-12 lg:px-6">
-      <h1 className="pl-6 text-xl font-medium">{item.name}</h1>
+    <div className="mx-auto mt-6 w-[22rem] md:w-[44rem] lg:w-[63rem]">
+      <h1 className="text-xl font-medium">{item.name}</h1>
       <div className="mt-3 grid grid-cols-1 items-center justify-items-center gap-2 md:grid-cols-2 lg:grid-cols-3">
         {item.children.map((tile) => (
           <Tile tile={tile} key={tile.link} />
