77fa71108756cbdb7d55a701b4862c9ebce885c7
Debbl <me@aiwan.run>
2023-04-17 11:22:17 +0800

    feat: add tool

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,9 +3,17 @@ import GithubIcon from "~/assets/images/github.svg";
 
 function Footer() {
   return (
-    <div className="flex justify-center py-6">
+    <div className="flex items-center justify-center py-6">
       <a href="https://github.com/Debbl/nav.aiwan.run">
         <Image src={GithubIcon} alt="" unoptimized></Image>
+      </a>
+      <a
+        href="https://dokidokiweb.com/"
+        target="_blank"
+        rel="noreferrer"
+        className="fixed right-2 bottom-0 opacity-0 hover:opacity-100"
+      >
+        tool
       </a>
     </div>
   );
