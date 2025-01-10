abbcfcac72ea2f0b68292329628d3c39034ced6f
Brendan Dash <me@aiwan.run>
2025-01-10 11:51:53 +0800

    feat: add shadcn

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "@debbl/eslint-config";
 
 export default defineConfig({
+  ignores: {
+    files: ["src/components/ui"],
+  },
   typescript: true,
   react: {
     next: true,
